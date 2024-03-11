6fb0af3072c21e746f825cbfaddf690db5c9dab6
Vishal <vishal779869@gmail.com>
2024-03-11 17:29:20 +0530

    url

--- src/app/constansts.ts
+++ src/app/constansts.ts
@@ -1,4 +1,4 @@
 
-// export const BASE_API_URL = 'https://qdtashrm-backend.onrender.com';
+export const BASE_API_URL = 'https://qdtashrm-backend.onrender.com';
 
-export const BASE_API_URL = 'http://localhost:8181'
+// export const BASE_API_URL = 'http://localhost:8181'
